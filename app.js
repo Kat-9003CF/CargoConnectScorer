7e2070c5f01d325d2117cbeab22328a285b724c5
Kat Hurdley <katherine.hurdley@googlemail.com>
2021-11-18 15:02:32 +0000

    Backend running

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', 5000);
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.json());
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({ title: 'Express' });
+router.get('/', function(req, res) {
+  res.send({ message: 'Welcome to the Scores server' });
+});
+
+
+// GET all Scores
+router.get("/Scores", async (req, res) => {
+  try {
+      let results = await db('SELECT * FROM Scores');
+      let scores = results.data;
+      res.send(scores);
+  } catch (err) {
+      res.status(500).send({ error: err.message });
+  }
 });
 
 module.exports = router;
